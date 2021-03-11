ea59803036651e623d27c6074b41d85744640792
cyfer2410 <00014220@uca.edu.sv>
2021-03-11 09:20:03 -0600

    Modify: example01

---
+++ example01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lab00
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a =0, b=0;
+
+            Console.WriteLine("Enter a value for a: ");
+            a=Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a value for b: ");
+            b=Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Adding {a} + {b} equals {addnumbers(a,b)} ");
+        }
+        static int addnumbers(int a, int b)
+        {
+            return a+b;
+        }
+    }
+}
